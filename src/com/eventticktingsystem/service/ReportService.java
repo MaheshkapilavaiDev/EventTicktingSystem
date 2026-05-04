561cc9bad03c48d2bf275b4ad0102bb604473c53
Mahesh Kumar <maheshkapilavai3230@gmail.com>
2026-05-04 08:47:51 +0530

    EventTicketingSystem New Changes

--- src/com/eventticktingsystem/service/ReportService.java
+++ src/com/eventticktingsystem/service/ReportService.java
@@ -7,21 +7,27 @@ import com.eventticktingsystem.model.Ticket;
 import com.eventticktingsystem.util.FileUtil;
 
 public class ReportService {
-
 	public void generateEventReport(String eventId, List<Ticket> tickets) {
 
-		List<Ticket> eventTickets = tickets.stream().filter(t -> t.getEventId().equals(eventId)).toList();
+		List<Ticket> eventTickets = tickets.stream().filter(t -> eventId.equals(t.getEventId())).toList();
 
 		double totalRevenue = eventTickets.stream().mapToDouble(Ticket::getTotalAmount).sum();
+
+		int totalSeats = eventTickets.stream().mapToInt(Ticket::getNumberOfSeats).sum();
 
 		StringBuilder report = new StringBuilder();
 		report.append("---- EVENT REPORT ----\n");
 		report.append("Event ID: ").append(eventId).append("\n");
 		report.append("Total Bookings: ").append(eventTickets.size()).append("\n");
+		report.append("Total Seats Booked: ").append(totalSeats).append("\n");
 		report.append("Total Revenue: ").append(totalRevenue).append("\n\n");
 
-		for (Ticket t : eventTickets) {
-			report.append(t.toString()).append("\n");
+		if (eventTickets.isEmpty()) {
+			report.append("No bookings found for this event.\n");
+		} else {
+			for (Ticket t : eventTickets) {
+				report.append(t.toString()).append("\n");
+			}
 		}
 
 		FileUtil.write("data/event_report_" + eventId + ".txt", report.toString());
@@ -29,3 +35,25 @@ public class ReportService {
 		System.out.println("Report generated!");
 	}
 }
+
+// public void generateEventReport(String eventId, List<Ticket> tickets) {
+
+/*
+ * List<Ticket> eventTickets = tickets.stream().filter(t ->
+ * t.getEventId().equals(eventId)).toList();
+ * 
+ * double totalRevenue =
+ * eventTickets.stream().mapToDouble(Ticket::getTotalAmount).sum();
+ * 
+ * StringBuilder report = new StringBuilder();
+ * report.append("---- EVENT REPORT ----\n");
+ * report.append("Event ID: ").append(eventId).append("\n");
+ * report.append("Total Bookings: ").append(eventTickets.size()).append("\n");
+ * report.append("Total Revenue: ").append(totalRevenue).append("\n\n");
+ * 
+ * for (Ticket t : eventTickets) { report.append(t.toString()).append("\n"); }
+ * 
+ * FileUtil.write("data/event_report_" + eventId + ".txt", report.toString());
+ * 
+ * System.out.println("Report generated!"); } }
+ */
